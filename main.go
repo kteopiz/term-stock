6eb2b7003db136a4350b83499aa7f98f42cd870f
Kyle Teopiz <kylecteopiz@gmail.com>
2026-07-26 20:27:12 -0400

    feat: Format tickers to TSX and fetch real time quote

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/wnjoon/go-yfinance/pkg/ticker"
+	"github.com/wnjoon/go-yfinance/pkg/utils"
+)
 
 func main() {
-  fmt.Println("Hello World!")
+	rawTicker := "H"
+	formattedTicker := utils.FormatYahooTicker(rawTicker, "XTSE")
+	t, err := ticker.New(formattedTicker)
+	if err != nil {
+		fmt.Println("uh oh", err)
+	}
+
+	quote, err := t.Quote()
+	if err != nil {
+		fmt.Println("uh oh 2", err)
+	}
+
+	fmt.Printf(" Name: %s, Symbol: %s\n Exchange: %s\n Market Price: %.2f\n Currency: %s\n Market State: %s",
+		quote.LongName,
+		quote.Symbol,
+		quote.ExchangeName,
+		quote.RegularMarketPrice,
+		quote.Currency,
+		quote.MarketState,
+	)
+
 }
